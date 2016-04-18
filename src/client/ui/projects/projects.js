dec2fe12eb34df9416330a8cc104e4f4b1a14cba
Jordon de Hoog <jordon.dehoog@gmail.com>
2016-04-18 16:59:30 -0400

    Add some data for the projects

--- src/client/ui/projects/projects.js
+++ src/client/ui/projects/projects.js
@@ -1,32 +1,44 @@
 const data = {
   items: [
     {
-      screenshot: '',
       title: 'Power Hour',
       subtitle: 'Drinking Game',
       tags: ['Android'],
-      description: 'A simple helper application to ease the playing of the classic drinking game Power Hour (also called Century Club).'
+      links: [
+        { text: 'Source', url: 'https://github.com/jordond/powerhour' },
+        { text: 'Play Store', url: 'https://play.google.com/store/apps/details?id=ca.hoogit.powerhour' }
+      ],
+      description: 'A simple helper application to ease the playing of the classic drinking game Power Hour (also called Century Club).  Designed to be clean and minimalistic, it features a very flat look.  The colours used can be completely customized, as well as a load of other settings.'
     },
     {
-      screenshot: '',
-      title: '',
-      subtitle: '',
-      tags: [],
-      description: ''
+      title: 'Hooold',
+      subtitle: 'Utility',
+      tags: ['Android'],
+      links: [
+        { text: 'Source', url: 'https://github.com/jordond/hooold' },
+        { text: 'Play Store', url: 'https://play.google.com/store/apps/details?id=ca.hoogit.hooold' }
+      ],
+      description: 'Have you always wanted the ability to schedule a message for a later date?  Maybe there is an important anniversary coming up, and if you miss it you\'ll be in big trouble.  With Hooold there is now an attractive and easy to use solution to this problem.'
     },
     {
-      screenshot: '',
-      title: '',
-      subtitle: '',
-      tags: [],
-      description: ''
+      title: 'Power Hour - Website',
+      subtitle: 'Landing page',
+      tags: ['Angular', 'ES6'],
+      links: [
+        { text: 'Source', url: 'https://github.com/jordond/powerhour-site' },
+        { text: 'Visit', url: 'https://powerhourapp.ca' }
+      ],
+      description: 'A relatively simple looking landing page for the Power Hour application.  It was designed to be as modular as possible.  Featuring a dead simple development workflow, followed with an easy to use deployment strategy when combined with my Docker image.'
     },
     {
-      screenshot: '',
-      title: '',
-      subtitle: '',
-      tags: [],
-      description: ''
+      title: 'Portfolio',
+      subtitle: 'Learn about me',
+      tags: ['Angular'],
+      links: [
+        { text: 'Source', url: 'https://github.com/jordond/resume-site' },
+        { text: 'Visit', url: 'https://jordondehoog.ca' }
+      ],
+      description: 'Learn everything you need to know about me and more!  Built with Angular and gulp, this was before I learned of the awesomeness that is module bundlers.'
     }
   ]
 };
